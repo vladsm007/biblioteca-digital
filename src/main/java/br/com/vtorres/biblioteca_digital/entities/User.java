9d0d0837ce135dd5b20a274abb861041da42a393
Victor Martins Torres <clerossephirot@gmail.com>
2025-03-26 08:54:39 -0300

    bugs corrigidos

--- src/main/java/br/com/vtorres/biblioteca_digital/entities/User.java
+++ src/main/java/br/com/vtorres/biblioteca_digital/entities/User.java
@@ -1,24 +1,23 @@
 package br.com.vtorres.biblioteca_digital.entities;
 
+
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
 
-@Setter
-@Getter
 @Entity
 @Table(name = "tb_users")
 @Data
+@Getter
+@Setter
 @ToString
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String email;
 
